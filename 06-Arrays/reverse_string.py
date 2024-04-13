67e54e80cda3abaf1edb91f9ecb48aa3dc69fe6a
Adi Malca <adimalca91@gmail.com>
2024-04-13 14:54:24 +0300

    Fixed a range parameter and added a new implementation

--- 06-Arrays/reverse_string.py
+++ 06-Arrays/reverse_string.py
@@ -5,10 +5,21 @@ Reverse a string
 def reverse(str):
     reverse_lst = []
     
-    for i in range(len(str)-1, 0, -1):
+    for i in range(len(str)-1, -1, -1):
         reverse_lst.append(str[i])
     
     reverse_str = ''.join(reverse_lst)
     return reverse_str
 
-        
+
+def reverse_str(str):
+    len_str = len(str)
+    reversed = ""
+    for i in range(len_str-1,-1,-1):
+        reversed += str[i]
+    return reversed
+
+
+print(reverse("hello this is me"))
+print()
+print(reverse_str("hello this is me"))
