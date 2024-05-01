965b82c113553bbec99e731a389c0b7217730bd9
Adi Malca <adimalca91@gmail.com>
2024-05-01 16:33:35 +0300

    Added more functionality

--- 07-LinkedLists/CircularLinkedLists.py
+++ 07-LinkedLists/CircularLinkedLists.py
@@ -42,6 +42,60 @@ class CircularLinkedList:
             new_node.next = self.head
             self.head = new_node
             
+            
+    def addLast(self,val):
+
+        new_node = Node(val)
+
+        if self.head == None:
+            self.head = new_node
+            new_node.next = self.head
+        else:
+            tmp = self.head
+            while tmp.next != self.head:
+                tmp = tmp.next
+                
+            new_node.next = self.head
+            tmp.next = new_node
+    
+    def search(self, key):
+        
+        if self.head == None:
+            return False
+        
+        tmp = self.head
+        
+        while True:
+            if tmp.data == key:
+                return True
+            tmp = tmp.next
+            if tmp == self.head:
+                break
+        return False
+    
+    def delete(self, key):
+        if self.head == None:                                           # Case 1: List is Empty
+            return
+        
+        if self.head.data == key and self.head.next == self.head:       # Case 2.1: When the Node to delete is the head and its the only node in the list
+            self.head = None
+            
+        elif self.head.data == key:                                    # Case 2.2: When the Node to delete is the head (the first node) and there are more nodes after it in the list
+            tmp = self.head
+            while tmp.next != self.head:
+                tmp = tmp.next
+                
+            tmp.next = self.head.next
+            self.head = self.head.next
+            
+        else:                                                         # Case 3: When the node to be deleted is any other node except the first node (the head)
+            cur = self.head
+            while cur.next != self.head:
+                if cur.next.data == key:
+                    cur.next = cur.next.next
+                    break
+                cur = cur.next            
+            
 
 if __name__ == '__main__':
     circular_list = CircularLinkedList()
@@ -63,6 +117,24 @@ if __name__ == '__main__':
     circular_list.printList()
     
     circular_list.addFirst(200)
+    
+    circular_list.printList()
+    
+    print("\nAdding a new node at the end of the list\n")
+    
+    circular_list.addLast(400)
+    
+    circular_list.addLast(500)
+    
+    circular_list.printList()
+    
+    print(circular_list.search(7))
+    
+    print(circular_list.search(20))
+    
+    print("\nDeleting a node from the list\n")
+    
+    circular_list.delete(10)
     
     circular_list.printList()
     
