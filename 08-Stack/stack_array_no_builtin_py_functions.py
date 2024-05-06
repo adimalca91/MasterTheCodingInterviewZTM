9993d7325fe27476736ed1d9ce63b9765e637f5e
Adi Malca <adimalca91@gmail.com>
2024-05-06 16:57:42 +0300

    Added an explaination about empty arrays

--- 08-Stack/stack_array_no_builtin_py_functions.py
+++ 08-Stack/stack_array_no_builtin_py_functions.py
@@ -25,14 +25,54 @@ class Stack():
             print("Stack is Empty")
         else:
             print(f"Popped element = {self.stack[self.top]}")
-            self.top = self.top - 1 
+            self.top = self.top - 1
+
+
+'''
+NOTE: You will get an IndexError: list assignment index out of range in line __ when trying to push to the stack
+(which is a Python List)
+
+This happens because stack is an empty list at the beginning, but you're attempting to write to element stack[0] , which doesn't exist yet.
+Same happens if we initialize self.stack = [None] and then try to push a second element.
+
+In short we need to have an initialization like above with [None] * size if we want to do the push without using append!
+
+Otherwise we can just solve it using append built in method!
+
+see explaination in the link below
+
+Also check out the Array implementation we have
+
+'''
         
+
+# https://stackoverflow.com/questions/5653533/why-does-this-iterative-list-growing-code-give-indexerror-list-assignment-index 
+
+
+# class Stack():
+    
+#     def __init__(self):
+#         self.stack = []
+#         self.top = -1
+#         self.size = 0
         
+#     def push(self, val):
+#         self.top = self.top + 1
+#         self.stack[self.top] = val    # NOTE: here we have a problem, to solve use append directly or the above code
+#         self.size += 1
+            
+#     def pop(self):
+#         if self.top == -1:
+#             print("Stack is Empty")
+#         else:
+#             print(f"Popped element = {self.stack[self.top]}")
+#             self.top = self.top - 1
+#             self.size -= 1 
         
 
 if __name__=='__main__':
 
-    s = Stack(3)
+    s = Stack(4)
 
     s.pop()
 
@@ -49,3 +89,23 @@ if __name__=='__main__':
     s.pop()
     s.pop()
     s.pop()
+
+    
+    # def test_arr(arr):
+    #     for i in range(len(arr)):
+    #         print(i)
+
+    #     if arr:
+    #         print("Has something - condition True")
+    #     else:
+    #         print("Has NOTHING - condition False")
+            
+    # arr = []
+     
+    # test_arr(arr)
+        
+    # arr.append(6)
+    
+    # test_arr(arr)
+    
+   
